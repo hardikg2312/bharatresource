75cb3bd3ddf8476becfd5b316cd85e2d61d2f114
Hardik Gondaliya <hardikg23@gmail.com>
2016-05-11 22:38:35 +0530

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   root 'workers#search'
 
+  get 'services', to: 'info#services', as: 'services'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
